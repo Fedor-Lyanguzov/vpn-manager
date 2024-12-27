35071d2b6fc643fde9e383deeffb6455d91ca93e
Fedor Lyanguzov <Fedor-Lyanguzov@users.noreply.github.com>
2024-12-27 23:53:58 +0300

    add static peer formatting for client config

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
-def test_true():
-    assert True
-    
+from vpn_manager import *
+
+def test_format_static_peer():
+    sp = StaticPeer('sample-public-key', '127.0.0.1', '12345')
+    peer_section = format_static_peer(sp, '0.0.0.0/0')
+    assert peer_section == '[Peer]\nPublicKey = sample-public-key\nAllowedIPs = 0.0.0.0/0\nEndpoint = 127.0.0.1:12345\nPersistentKeepAlive = 30'
--- vpn_manager/__main__.py
+++ vpn_manager/__main__.py
@@ -1,3 +1,21 @@
+from dataclasses import dataclass
+
+@dataclass
+class StaticPeer:
+    public_key: str
+    endpoint: str
+    port: str
+
+
+def format_static_peer(static_peer, routes, keepalive=30):
+    return f'''[Peer]
+PublicKey = {static_peer.public_key}
+AllowedIPs = {routes}
+Endpoint = {static_peer.endpoint}:{static_peer.port}
+PersistentKeepAlive = {keepalive}'''
+    
+
+
 def main():
     print("run")
 
