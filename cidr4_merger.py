9d5c6b384a109a4d0c6b339fa1608bcd67e7734d
Павел <patsey.pavel@gmail.com>
2025-01-11 23:59:29 +0300

    add algorithm

--- cidr4_merger.py
+++ cidr4_merger.py
@@ -1,4 +1,5 @@
 import cProfile
+import math
 from typing import Optional
 
 Node = tuple[int, int, int]
@@ -46,7 +47,7 @@ def have_same_parent(a: Node, b: Node) -> bool:
 
 def get_group_with_max_mask_len(nodes: list[Node]) -> list[Node]:
     max_mask_len = max(nodes, key=lambda x: x[1])[1]
-    return sort_nodes(filter(lambda x: x[1] == max_mask_len, nodes))
+    return list(filter(lambda x: x[1] == max_mask_len, nodes))
 
 
 def get_parent(a: Node, b: Node = None) -> Node:
@@ -97,24 +98,53 @@ def reduce_nodes(nodes: list[Node]) -> list[Node]:
 
 
 def merge_nodes(nodes: list[Node], required_len: int) -> list[Node]:
-    pass
+    while len(nodes) > required_len:
+        nodes = reduce_nodes(nodes)
+    return nodes
+
+
+def node_to_cidr4(node: Node) -> str:
+    ip_value, mask_len, _ = node
+    lst = [str(ip_value >> (i << 3) & 0xFF) for i in reversed(range(4))]
+    ip = ".".join(lst)
+    return f"{ip}/{mask_len}"
+
+
+def answer(nodes: list[Node], required_len: int) -> tuple[list[str], int]:
+    nodes = sort_nodes(nodes)
+    merged_nodes = merge_nodes(nodes, required_len)
+
+    cidr4s = []
+    sum_added_ips = 0
+    for node in merged_nodes:
+        cidr4s.append(node_to_cidr4(node))
+        sum_added_ips += node[2]
+    return cidr4s, sum_added_ips
 
 
 def main():
     file = "cidr4.txt"
-    required_len = 15
+    required_len = 20
 
     data = get_data(file)
     nodes = data_to_nodes(data)
-    # for n in nodes:
-    #     print(n)
+    cidr4s, sum_added_ips = answer(nodes, required_len)
 
-    merged_nodes = merge_nodes(nodes, required_len)
+    cidr4s_str = "\n".join(cidr4s)
+    print(
+        f"Исходный список длины {len(nodes)} сокращен до {len(cidr4s)}\n"
+        f"Количество добавленных ip адресов: {sum_added_ips:_}\n"
+        f"Список объединенных cidr4:\n"
+        f"{cidr4s_str}"
+    )
 
 
 if __name__ == "__main__":
     assert cidr4_to_node("4.78.139.0/24") == (72256256, 24, 0)
     assert cidr4_to_node("0.0.0.0/32") == (0, 32, 0)
+
+    assert node_to_cidr4((72256256, 24, 0)) == "4.78.139.0/24"
+    assert node_to_cidr4((0, 32, 10)) == "0.0.0.0/32"
 
     bin_a = "10011000000000001000010000010000"
     assert len(bin_a) == 32
@@ -183,25 +213,21 @@ if __name__ == "__main__":
 
     assert reduce_nodes(
         [
-            (0, 0, 0),
             (0, 2, 12),
             (1073741824, 2, 3),
         ]
     ) == [
-        (0, 0, 0),
         (0, 1, 15),
     ]
 
     assert reduce_nodes(
         [
-            (0, 0, 0),
             (0, 2, 12),
             (1073741824, 2, 3),
             (2147483648, 2, 1),
             (3221225472, 2, 2),
         ]
     ) == [
-        (0, 0, 0),
         (2147483648, 1, 3),
         (0, 2, 12),
         (1073741824, 2, 3),
@@ -226,5 +252,42 @@ if __name__ == "__main__":
         (0, 0, 12 + 2**30),
     ]
 
-    # main()
-    # cProfile.run("main()")
+    assert merge_nodes(
+        [
+            (0, 2, 12),
+            (2147483648, 2, 1),
+            (3221225472, 2, 2),
+        ],
+        2,
+    ) == [
+        (2147483648, 1, 3),
+        (0, 2, 12),
+    ]
+
+    assert merge_nodes(
+        [
+            (0, 2, 12),
+            (2147483648, 2, 1),
+            (3221225472, 2, 2),
+        ],
+        1,
+    ) == [(0, 0, 15 + 2**30)]
+
+    assert answer(
+        [
+            (0, 2, 0),
+            (2147483648, 2, 0),
+            (3221225472, 2, 12),
+        ],
+        2,
+    ) == (["128.0.0.0/1", "0.0.0.0/2"], 12)
+
+    assert answer(
+        [
+            (0, 2, 0),
+            (2147483648, 2, 0),
+        ],
+        1,
+    ) == (["0.0.0.0/0"], 2**31)
+
+    cProfile.run("main()")
