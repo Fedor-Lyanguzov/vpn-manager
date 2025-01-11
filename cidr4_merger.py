9377d2313430252ac574e1df7e0727e5e99d64a1
Павел <patsey.pavel@gmail.com>
2025-01-11 08:08:08 +0300

    add convert data to nodes

--- cidr4_merger.py
+++ cidr4_merger.py
@@ -1,8 +1,6 @@
 import cProfile
-from collections import defaultdict
-from copy import deepcopy
-from ipaddress import IPv4Address
-from typing import List, Set, Tuple
+
+Node = tuple[int, int, int]
 
 
 def get_data(input_file):
@@ -11,14 +9,17 @@ def get_data(input_file):
     return data
 
 
-def cidr4_to_node(cidr4: str) -> Tuple[int, int, int]:
+def cidr4_to_node(cidr4: str) -> Node:
     ip, mask_len = cidr4.strip().split("/")
     mask_len = int(mask_len)
     added_ips_number = 0
-    a, b, c, d = ip.split(".")
-    a, b, c, d = int(a), int(b), int(c), int(d)
+    a, b, c, d = list(map(int, ip.split(".")))
     ip_value = a * 256**3 + b * 256**2 + c * 256**1 + d * 256**0
     return ip_value, mask_len, added_ips_number
+
+
+def data_to_nodes(data: list[str]) -> list[Node]:
+    return sorted(map(cidr4_to_node, data))
 
 
 def main():
@@ -26,13 +27,14 @@ def main():
     required_len = 15
 
     data = get_data(file)
-    nodes = sorted(map(cidr4_to_node, data))
+    nodes = data_to_nodes(data)
     for n in nodes:
         print(n)
 
 
 if __name__ == "__main__":
     assert cidr4_to_node("4.78.139.0/24") == (72256256, 24, 0)
+    assert cidr4_to_node("0.0.0.0/32") == (0, 32, 0)
 
     main()
     # cProfile.run("main()")
