0b56caffac0bdc6548215bf2b59d5c39d2e72d96
Pavel Patsey <patsey.pavel@gmail.com>
2025-01-26 10:52:45 +0300

    fix tests after removing functions

--- tests/cidr4_merge/test_cidr4_merger.py
+++ tests/cidr4_merge/test_cidr4_merger.py
@@ -1,8 +1,6 @@
 from vpn_manager.cidr4_merge.cidr4_merger import (
-    calc_dip,
     cidr4_to_node,
     find_neighbors,
-    find_parent,
     make_cidr4,
     merge_nodes,
     solution,
@@ -36,30 +34,6 @@ def test_make_cidr4():
     assert make_cidr4(2899902464, 19) == "172.217.0.0/19"
     assert make_cidr4(400657664, 24) == "23.225.141.0/24"
     assert make_cidr4(520969728, 23) == "31.13.94.0/23"
-
-
-def test_find_parent():
-    assert find_parent((0, 2), (1073741824, 2)) == (0, 1)
-    assert find_parent((2147483648, 2), (3221225472, 2)) == (2147483648, 1)
-    assert find_parent((0, 2), (3221225472, 2)) == (0, 0)
-    assert find_parent((1, 32), (6, 32)) == (0, 29)
-
-    assert find_parent((0, 32), (0, 29)) == (0, 29)
-    assert find_parent((0, 1), (1073741824, 2)) == (0, 1)
-    assert find_parent((0, 0), (3221225472, 2)) == (0, 0)
-
-
-def test_calc_dip():
-    assert calc_dip(32, 32, 31) == 0
-    assert calc_dip(32, 32, 30) == 2
-    assert calc_dip(32, 32, 29) == 6
-    assert calc_dip(32, 30, 29) == 3
-    assert calc_dip(32, 31, 29) == 5
-    assert calc_dip(32, 31, 29) == 5
-
-    assert calc_dip(2, 2, 1) == 0
-    assert calc_dip(2, 2, 0) == 2**31
-    assert calc_dip(3, 3, 1) == 2**30
 
 
 def test_merge_nodes():
